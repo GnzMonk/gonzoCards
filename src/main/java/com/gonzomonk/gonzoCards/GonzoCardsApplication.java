1cc6dec4ccd59dc697f9b6853608d61786c56768
Vladislav Sivikhin <sivlad004@yandex.ru>
2026-03-02 17:37:11 +0300

    initial commit

---
+++ src/main/java/com/gonzomonk/gonzoCards/GonzoCardsApplication.java
@@ -0,0 +1,13 @@
+package com.gonzomonk.gonzoCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzoCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonzoCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzomonk/gonzoCards/GonzoCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzomonk.gonzoCards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonzoCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
